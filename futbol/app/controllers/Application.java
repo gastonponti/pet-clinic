2dc472118f7003cd6980b153f86723ab6c8bb697
santi-pc <santi-pc@santipc-Inspiron-N5110.(none)>
2012-05-05 16:21:02 -0300

    Se agrega auth

--- futbol/app/controllers/Application.java
+++ futbol/app/controllers/Application.java
@@ -2,19 +2,68 @@ package controllers;
 
 import models.Player;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+
+    public static class Login {
+        
+        public String email;
+        public String password;
+        
+        public String validate() {
+            if(Player.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
+        
+    }
+
+    /**
+     * Login page.
+     */
+    public static Result login() {
+        return ok(
+            login.render(form(Login.class))
+        );
+    }
+    
+    /**
+     * Handle login form submission.
+     */
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if(loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session("email", loginForm.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
+
+    /**
+     * Logout and clean the session.
+     */
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+            routes.Application.login()
+        );
+    }
+	
+    public static Result index() {
+    	return ok(index.render("Your new application is ready."));
+    }
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
-  
-  public static Result playersList() {
-	  return ok(views.html.admin.render(Player.findAll()));
-  }
+    public static Result playersList() {
+    	return ok(views.html.admin.render(Player.findAll()));
+    }
   
 }
